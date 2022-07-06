40d2e6755e6799a6fd615df597a1f1d025715a58
Aman <89300739+aman8094@users.noreply.github.com>
2022-07-06 08:37:57 +0530

    1_Starter

---
+++ Amanjava2022/src/com/Aman/_16FunctionOverloading.java
@@ -0,0 +1,16 @@
+package com.Aman;
+
+public class _16FunctionOverloading {
+    public static void main(String[] args) {
+        fun(8);
+        fun("aman");
+
+    }
+    static void fun(int a){
+        System.out.println(a);
+
+    }
+    static void fun(String b){
+        System.out.println(b);
+    }
+}
